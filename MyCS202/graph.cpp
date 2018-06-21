42b2138d4de0d85cedf1740bfd21f7b7f71c2188
raul zarate <zarater.raul@gmail.com>
2018-06-21 10:19:52 -0700

    'added a declaration'

---
+++ MyCS202/graph.cpp
@@ -0,0 +1,23 @@
+#include "inh.h"
+
+const int MAXSIZE;
+
+class graph
+{
+	public:
+	
+	int mappull();
+	
+	protected:
+	int speedlimit;
+	char * neighborarray;
+
+};
+
+
+int graph :: mapppull()
+{
+	
+	
+	return 1;
+}
