6a1bee3fcda037ff78dcd8d9ab2a9eb8a1ae54fc
Raul Zarate <rzarate@uss-hd4-mac.psu.ds.pdx.edu>
2018-06-21 15:38:00 -0700

    added grid shell and added some stuff that doesnt compile in graph.cpp

--- MyCS202/graph.cpp
+++ MyCS202/graph.cpp
@@ -1,23 +1,67 @@
 #include "inh.h"
+#include <fstream>
 
-const int MAXSIZE;
+const int MAXSIZE = 100;
+class vertex
+{
+	public:
+	vertex();
+	~vertex();
+	int setVertexRow();
+
+	protected:
+	int row;
+};
+vertex :: vertex(): row(0)
+{}
+vertex :: ~vertex()
+{}
+int vertex :: setVertexRow()
+{
+	return 1;
+}
 
 class graph
 {
 	public:
-	
+	graph();
+	~graph();
 	int mappull();
 	
 	protected:
 	int speedlimit;
-	char * neighborarray;
+	char * neighbor;
+	vertex *vertextrow;
 
 };
 
+graph::graph(): speedlimit(0), neighbor(NULL)
+{}
+graph :: ~graph()
+{}
 
-int graph :: mapppull()
+
+int graph::mapppull()
 {
-	
-	
-	return 1;
+	int x = 0;
+	ifstream file_in;
+	file_in.open("grid.txt");
+	if(!file_in)
+	{
+		cout << "Not able to load grid" <<endl;
+	}
+	while(!file_in.eof())
+	{
+		//file_in >> speed; 
+		file_in.get(speedlimit, :);
+		file_in.ignore();
+		//file_in.ignore();
+		vertexRow[x].setVertexRow(neighbor);
+		++x;
+	}
+	file_in.close();
+}
+
+
+		return 1;
 }
